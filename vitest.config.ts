a78a4462db124aac04cfc873d9ba0a9ad1ebeeb1
Jheff4 <ogbevoenetinosa@gmail.com>
2026-03-21 10:59:16 +0100

    Updated dependencies, added testing libraries, and configured Vitest with React support

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'vitest/config';
 
+import react from "@vitejs/plugin-react"
+
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 
 import { playwright } from '@vitest/browser-playwright';
@@ -20,6 +22,7 @@ export default defineConfig({
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
           storybookTest({ configDir: path.join(dirname, '.storybook') }),
+          react()
         ],
         test: {
           name: 'storybook',
@@ -29,6 +32,9 @@ export default defineConfig({
             provider: playwright({}),
             instances: [{ browser: 'chromium' }],
           },
+          environment: "jsdom",
+          globals: true,
+          setupFiles: "src/tests/setup.ts"
         },
       },
     ],
